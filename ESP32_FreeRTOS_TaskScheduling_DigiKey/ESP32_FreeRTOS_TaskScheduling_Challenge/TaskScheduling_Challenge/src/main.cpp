58ed36afb0a92a11da06b49749dd966f7aa50f9a
Saeed Bazargan <s.bazargan@tosantechno.net>
2024-08-12 14:11:37 +0330

    I did the challenge and you can delay the LED blinking time. Web:https://www.digikey.com/en/maker/projects/introduction-to-rtos-solution-to-part-3-task-scheduling/8fbb9e0b0eed4279a2dd698f02ce125f

--- ESP32_FreeRTOS_TaskScheduling_DigiKey/ESP32_FreeRTOS_TaskScheduling_Challenge/TaskScheduling_Challenge/src/main.cpp
+++ ESP32_FreeRTOS_TaskScheduling_DigiKey/ESP32_FreeRTOS_TaskScheduling_Challenge/TaskScheduling_Challenge/src/main.cpp
@@ -7,22 +7,71 @@
   static const BaseType_t app_cpu = 1;
 #endif
 //<---- ------------------------------------------------------------------ ---->
+#define lengthOfNumber              5
+#define led_pin                     LED_BUILTIN
 //<---- ------------------------------------------------------------------ ---->
-char recNum_c[10];
-uint8_t recNum_i;
+char recNum_c[lengthOfNumber];
+uint8_t counterOfRecNumber = 0;
+static int realNumber = 500;
 //<---- ------------------------------------------------------------------ ---->
+static TaskHandle_t recTask = NULL;
+static TaskHandle_t toggleTask = NULL;
+
+void startReceiveTask(void* parameter);
+void startToggleLEDTask(void* parameter);
 //<---- ------------------------------------------------------------------ ---->
 void setup()
 {
   Serial.begin(300);
   Serial.println("<---- Multi-task LED Demo ---->");
   Serial.println("<---- Enter a number in milliseconds to change the LED delay ---->");
+
+  pinMode(led_pin, OUTPUT);
+  
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+  xTaskCreatePinnedToCore(startReceiveTask, "REC_TASK", 1024, NULL, 1, &recTask, app_cpu);
+  xTaskCreatePinnedToCore(startToggleLEDTask, "TOGGLE_TASK", 1024, NULL, 1, &toggleTask, app_cpu);
+  vTaskDelete(NULL);
 }
 //<---- ------------------------------------------------------------------ ---->
 void loop()
+{}
+//<---- ------------------------------------------------------------------ ---->
+void startReceiveTask(void* parameter)
 {
-  if(Serial.available())
+  while (1)
   {
+    if(Serial.available())
+    {
+      recNum_c[++counterOfRecNumber] = Serial.read();
+      Serial.print(recNum_c[counterOfRecNumber]);
+
+      if(counterOfRecNumber > 10)
+      {
+        Serial.println("Wrong Number!");
+      }
+      else if(recNum_c[counterOfRecNumber] == '\n')
+      {
+        realNumber = atoi(&recNum_c[1]);
+        Serial.print("your number is: ");
+        Serial.println(realNumber);
+
+        memset(recNum_c, 0, lengthOfNumber);
+        counterOfRecNumber = 0;
+      }
+    }
   }
+}
+//<---- ------------------------------------------------------------------ ---->
+void startToggleLEDTask(void* parameter)
+{
+  while (1)
+  {
+    digitalWrite(led_pin, HIGH);
+    vTaskDelay(realNumber / portTICK_PERIOD_MS);
+    digitalWrite(led_pin, LOW);
+    vTaskDelay(realNumber / portTICK_PERIOD_MS);
+  }  
 }
 //<---- ------------------------------------------------------------------ ---->
