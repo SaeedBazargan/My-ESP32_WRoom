dd2a8b76b2b225e08000ce1cf40fc5ba9057591f
Saeed Bazargan <s.bazargan@tosantechno.net>
2024-08-20 16:30:43 +0330

    Add FreeRTOS queue and serial input handling in Task 1
    - Implement Task 1 to handle serial input and process commands.
    - Create two FreeRTOS queues (Queue_1 and Queue_2) for task communication.
    - Add functionality to read and store characters from serial input.
    - Implement command processing for 'delay' command with queue integration.
    - Stub for Task 2 created but not yet implemented.

--- ESP32_FreeRTOS_Queue_DigiKey/ESP32_FreeRTOS_Queue_Challenge/Queue_Challenge/src/main.cpp
+++ ESP32_FreeRTOS_Queue_DigiKey/ESP32_FreeRTOS_Queue_Challenge/Queue_Challenge/src/main.cpp
@@ -31,8 +31,11 @@ void setup()
 
   vTaskDelay(1000 / portTICK_PERIOD_MS);
 
+  Queue_1 = xQueueCreate(msg_queue_length, sizeof(int));
+  Queue_2 = xQueueCreate(msg_queue_length, sizeof(int));
+
   xTaskCreatePinnedToCore(startTask1, "TASK_1", 1024, NULL, 1, &Task_1, app_cpu);
-  xTaskCreatePinnedToCore(startTask2, "TASK_2", 1024, NULL, 1, &Task_2, app_cpu);
+  // xTaskCreatePinnedToCore(startTask2, "TASK_2", 1024, NULL, 1, &Task_2, app_cpu);
 }
 //<---- --------------------------------------------------------- ---->
 void loop()
@@ -57,19 +60,24 @@ void startTask1(void* parameter)
 
       if(counterOfRecChar > 49)
       {
-        Serial.println("Write Shoerter than 50 words!");
+        Serial.println(" Write Shorter than 50 words!");
         
         memset(recChar_c, 0, lengthOfChar);
         counterOfRecChar = 0;
       }
       else if(recChar_c[counterOfRecChar] == '\n')
       {
-        if(memcmp(recChar_c, "delay ", 6))
-        {}
-        else
+        Serial.print("your character is: ");
+        Serial.println(&recChar_c[1]);
+
+        if(memcmp(&recChar_c[1], "delay ", 6) == 0)
         {
-          Serial.print("your character is: ");
-          Serial.println(recChar_c);
+          int tmp = atoi(&recChar_c[7]);
+          
+          if(xQueueSend(Queue_1, (void*)&tmp, 0) != pdTRUE)
+          {
+            Serial.print("you can not send delay time to Queue_1!");
+          }
         }
 
         memset(recChar_c, 0, lengthOfChar);
