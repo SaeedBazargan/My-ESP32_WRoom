df7384964d396ad762896fa5a3b8f94a97c20fc2
Saeed Bazargan <Bazargan0241@hotmail.com>
2024-08-26 20:45:24 +0330

    Add ISR and FreeRTOS task for critical section handling on ESP32
    - Configured ESP32 hardware timer to trigger ISR at 100ms intervals.
    - Implemented ISR with critical section to increment a shared counter.
    - Added FreeRTOS task to safely read and print counter value using critical sections.
    - Introduced delay to print task to demonstrate synchronized counter access.

--- ESP32_FreeRTOS_HardwareInterrupt_and_Timer_DigiKey/ESP32_FreeRTOS_HwInterrupt/src/main.cpp
+++ ESP32_FreeRTOS_HardwareInterrupt_and_Timer_DigiKey/ESP32_FreeRTOS_HwInterrupt/src/main.cpp
@@ -8,18 +8,24 @@
 
 #define led_pin                     LED_BUILTIN
 //<---- ------------------------------------------------------------------ ---->
-static const uint16_t timer_divider = 80;   // Clock tick at 1 MHz (ESP32 Timer = 80MHz, So timer_divider = 80 it means 1 Sec)
+static const uint16_t timer_divider = 8;   // Clock tick at 10 MHz (ESP32 Timer = 80MHz, So timer_divider = 8 it means 100 milliSec)
 static const uint64_t timer_max_count = 1000000;
 
+static const TickType_t delay_time = 2000 / portTICK_PERIOD_MS;
+
 static hw_timer_t *hal_timer = NULL;
+static volatile int isr_counter;
+static portMUX_TYPE spinlock = portMUX_INITIALIZER_UNLOCKED;
 //<---- ------------------------------------------------------------------ ---->
 void IRAM_ATTR onTimer();
+void print_values(void* parameters);
 //<---- ------------------------------------------------------------------ ---->
 //<---- ------------------------------------------------------------------ ---->
 
 void setup() 
 {
-  pinMode(led_pin, OUTPUT);
+  Serial.begin(300);
+  Serial.println("<--- FreeRTOS ISR Critical Section Demo ---->");
 
   // Create and Start timer (timer_number, divider, countUp)
   hal_timer = timerBegin(0, timer_divider, true);
@@ -32,6 +38,10 @@ void setup()
 
   //Allow ISR to trigger
   timerAlarmEnable(hal_timer);
+
+  xTaskCreatePinnedToCore(print_values, "PRINT_TASK", 1024, NULL, 1, NULL, app_cpu);
+
+  vTaskDelete(NULL);
 }
 //<---- ------------------------------------------------------------------ ---->
 void loop()
@@ -39,6 +49,25 @@ void loop()
 //<---- ------------------------------------------------------------------ ---->
 void IRAM_ATTR onTimer()  // This ISR function executes when timer reaches max and resets
 {
-  uint8_t pinstate = digitalRead(led_pin);
-  digitalWrite(led_pin, !pinstate);
+  portENTER_CRITICAL_ISR(&spinlock);
+  isr_counter++;
+  portEXIT_CRITICAL_ISR(&spinlock);
 }
+//<---- ------------------------------------------------------------------ ---->
+void print_values(void* parameters)   // wait for semaphore and print out value when received 
+{
+  while (1)
+  {
+    while (isr_counter > 0)
+    {
+      Serial.println(isr_counter);
+
+      portENTER_CRITICAL_ISR(&spinlock);
+      isr_counter--;
+      portEXIT_CRITICAL_ISR(&spinlock);
+    }
+    
+    vTaskDelay(delay_time);
+  }  
+}
+
