a5514d56efd778dcb4dec7a15fd56b91260d91b2
Saeed Bazargan <Bazargan0241@hotmail.com>
2024-08-30 13:09:43 +0330

    Add FreeRTOS task and hardware timer ISR for user input handling on ESP32
    - Implemented a task to capture serial input and convert it to an integer value.
    - Configured a hardware timer to trigger an ISR after 10 seconds based on user input.
    - Implemented ISR to notify the task when the timer expires, toggling an LED and printing a message.
    - Added input validation and handling to ensure correct input length and format.

---
+++ ESP32_FreeRTOS_HardwareInterrupt_and_Timer_DigiKey/ESP32_FreeRTOS_HwInterrupt_Challenge/HwInterrupt_Challenge/src/main.cpp
@@ -0,0 +1,94 @@
+#include <Arduino.h>
+
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
+#endif
+//<---- ------------------------------------------------------------------ ---->
+#define lengthOfNumber              5
+#define led_pin                     LED_BUILTIN
+//<---- ------------------------------------------------------------------ ---->
+char recNum_c[lengthOfNumber];
+uint8_t counterOfRecNumber = 0;
+static int realNumber = 500;
+
+static const uint16_t timer_divider = 800;   // Clock tick at 10 MHz (ESP32 Timer = 80MHz, So timer_divider = 800 it means 10 Sec)
+static const uint64_t timer_max_count = 1000000;
+
+volatile uint8_t recordCount = 0;
+//<---- ------------------------------------------------------------------ ---->
+static TaskHandle_t recordTask_Handle = NULL;
+
+static hw_timer_t *hal_timer = NULL;
+//<---- ------------------------------------------------------------------ ---->
+void recordSerial_Task(void* parameters);
+void IRAM_ATTR timer_isr_handler();
+//<---- ------------------------------------------------------------------ ---->
+//<---- ------------------------------------------------------------------ ---->
+
+void setup() 
+{
+  Serial.begin(300);
+  Serial.println("<---- FreeRTOS Hardware Interrupt Demo ---->");
+  Serial.println("<---- Enter some numbers to calculate the average! ---->");
+
+  pinMode(led_pin, OUTPUT);
+
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+  xTaskCreatePinnedToCore(recordSerial_Task, "RECORD_TASK", 1024, NULL, 1, &recordTask_Handle, app_cpu);
+
+  vTaskDelete(NULL);
+}
+//<---- ------------------------------------------------------------------ ---->
+void loop() 
+{}
+//<---- ------------------------------------------------------------------ ---->
+void recordSerial_Task(void* parameters)
+{
+  uint32_t ulNotifyValue;
+
+  while (1)
+  {
+    ulNotifyValue = ulTaskNotifyTake(pdTRUE, 0);
+    if(ulNotifyValue != 0)
+    {
+      digitalWrite(led_pin, HIGH);
+      Serial.println("Hello from Timer_ISR function");
+    }
+
+    if(Serial.available())
+    {
+      recNum_c[++counterOfRecNumber] = Serial.read();
+      Serial.print(recNum_c[counterOfRecNumber]);
+
+      if(counterOfRecNumber > 10)
+      {
+        Serial.println("Wrong Number!");
+      }
+      else if(recNum_c[counterOfRecNumber] == '\n')
+      {
+        realNumber = atoi(&recNum_c[1]);
+
+        hal_timer = timerBegin(0, timer_divider, true);               // Create and Start timer (timer_number, divider, countUp)
+        timerAttachInterrupt(hal_timer, &timer_isr_handler, true);    // Provide ISR to timer (timer, void (*function)(), edge)
+        timerAlarmWrite(hal_timer, timer_max_count, false);           // At what count should ISR trigger (timer, count, autoreload)
+        timerAlarmEnable(hal_timer);                                  // Allow ISR to trigger
+
+        memset(recNum_c, 0, lengthOfNumber);
+        counterOfRecNumber = 0;
+      }
+    }
+  }
+}
+//<---- ------------------------------------------------------------------ ---->
+void IRAM_ATTR timer_isr_handler()
+{
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  vTaskNotifyGiveFromISR(recordTask_Handle, &xHigherPriorityTaskWoken);
+  if(xHigherPriorityTaskWoken==pdTRUE)
+  {
+    portYIELD_FROM_ISR();
+  }
+}
