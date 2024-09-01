c86d77f3640cedf6bd36ac3f59a4e70731d50793
Saeed Bazargan <s.bazargan@tosantechno.net>
2024-09-01 11:21:14 +0330

    Add ISR-driven task for averaging user input with queue and ring buffer integration
    - Implemented a task to record and process serial input, storing integers in a ring buffer.
    - Added a hardware timer with ISR to trigger processing of buffered input after a 10-second delay.
    - Introduced a new task to calculate and print the average of the numbers received, using a queue for inter-task communication.
    - Enhanced error handling for invalid input and buffer overflow.

--- ESP32_FreeRTOS_HardwareInterrupt_and_Timer_DigiKey/ESP32_FreeRTOS_HwInterrupt_Challenge/HwInterrupt_Challenge/src/main.cpp
+++ ESP32_FreeRTOS_HardwareInterrupt_and_Timer_DigiKey/ESP32_FreeRTOS_HwInterrupt_Challenge/HwInterrupt_Challenge/src/main.cpp
@@ -9,21 +9,31 @@
 //<---- ------------------------------------------------------------------ ---->
 #define lengthOfNumber              5
 #define led_pin                     LED_BUILTIN
+#define msg_queue_length            10
 //<---- ------------------------------------------------------------------ ---->
 char recNum_c[lengthOfNumber];
-uint8_t counterOfRecNumber = 0;
+uint8_t counterOfRecCharacter = 0;
 static int realNumber = 500;
+
+typedef struct 
+{
+  char message[20];  // "Average: " message
+  int Number;        // The calculated average value
+} AvgData_t;
 
 static const uint16_t timer_divider = 800;   // Clock tick at 10 MHz (ESP32 Timer = 80MHz, So timer_divider = 800 it means 10 Sec)
 static const uint64_t timer_max_count = 1000000;
 
 volatile uint8_t recordCount = 0;
 //<---- ------------------------------------------------------------------ ---->
+static QueueHandle_t avg_msg_queue;
 static TaskHandle_t recordTask_Handle = NULL;
+static TaskHandle_t echoTask_Handle = NULL;
 static RingbufHandle_t buffer_Handle = NULL;
 static hw_timer_t *hal_timer = NULL;
 //<---- ------------------------------------------------------------------ ---->
 void recordSerial_Task(void* parameters);
+void echoSerial_Task(void* parameters);
 void IRAM_ATTR timer_isr_handler();
 //<---- ------------------------------------------------------------------ ---->
 //<---- ------------------------------------------------------------------ ---->
@@ -38,10 +48,13 @@ void setup()
 
   vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-  buffer_Handle = xRingbufferCreate(10, RINGBUF_TYPE_BYTEBUF);
+  buffer_Handle = xRingbufferCreate(1024, RINGBUF_TYPE_BYTEBUF);
   
-  xTaskCreatePinnedToCore(recordSerial_Task, "RECORD_TASK", 1024, NULL, 1, &recordTask_Handle, app_cpu);
+  avg_msg_queue = xQueueCreate(msg_queue_length, sizeof(AvgData_t));
 
+  xTaskCreatePinnedToCore(recordSerial_Task, "RECORD_TASK", 1024, NULL, 1, &recordTask_Handle, app_cpu);
+  xTaskCreatePinnedToCore(echoSerial_Task, "ECHO_TASK", 1024, NULL, 1, &echoTask_Handle, app_cpu);
+  
   vTaskDelete(NULL);
 }
 //<---- ------------------------------------------------------------------ ---->
@@ -50,9 +63,14 @@ void loop()
 //<---- ------------------------------------------------------------------ ---->
 void recordSerial_Task(void* parameters)
 {
+  AvgData_t send_AvgData;
   uint32_t ulNotifyValue;
+  bool timer_flag = false;
 
-  int* recMsg;
+  int *receivedData;
+  size_t item_size;
+
+  uint8_t counterOfNumbers = 0;
 
   while (1)
   {
@@ -60,36 +78,71 @@ void recordSerial_Task(void* parameters)
     if(ulNotifyValue != 0)
     {
       digitalWrite(led_pin, HIGH);
-      Serial.println("Hello from Timer_ISR function");
+      Serial.println("<---- Hello from Timer_ISR function ---->");
 
-      size_t receivedMessageSize;
-      recMsg = (int*)xRingbufferReceiveUpTo(buffer_Handle, &receivedMessageSize, 0, 10);
-      Serial.println(*recMsg);
+      // Receive data from the ring buffer
+      receivedData = (int*)xRingbufferReceive(buffer_Handle, &item_size, 0);
+
+      for (uint8_t i = 0; i < counterOfNumbers; i++)
+      {
+        send_AvgData.Number += receivedData[i];
+      }
+
+      strcpy(send_AvgData.message, "Average: ");
+      send_AvgData.Number /= counterOfNumbers;
+
+      // Send the struct to the queue
+      if(xQueueSend(avg_msg_queue, (void*)&send_AvgData, 0) == pdTRUE)
+      {
+        Serial.println("<---- The message has been sent to the queue ---->");
+      }
     }
 
     if(Serial.available())
     {
-      recNum_c[++counterOfRecNumber] = Serial.read();
-      Serial.print(recNum_c[counterOfRecNumber]);
+      recNum_c[++counterOfRecCharacter] = Serial.read();
+      Serial.print(recNum_c[counterOfRecCharacter]);
 
-      if(counterOfRecNumber > 10)
+      if(counterOfRecCharacter > 10)
       {
-        Serial.println("Wrong Number!");
+        Serial.println("<---- Wrong Number! ---->");
       }
-      else if(recNum_c[counterOfRecNumber] == '\n')
+      else if(recNum_c[counterOfRecCharacter] == '\n')
       {
+        counterOfNumbers++;
         realNumber = atoi(&recNum_c[1]);
-        
+
+        // Send the byte to the ring buffer
         xRingbufferSend(buffer_Handle, &realNumber, sizeof(realNumber), 0);
 
-        hal_timer = timerBegin(0, timer_divider, true);               // Create and Start timer (timer_number, divider, countUp)
-        timerAttachInterrupt(hal_timer, &timer_isr_handler, true);    // Provide ISR to timer (timer, void (*function)(), edge)
-        timerAlarmWrite(hal_timer, timer_max_count, false);           // At what count should ISR trigger (timer, count, autoreload)
-        timerAlarmEnable(hal_timer);                                  // Allow ISR to trigger
+        if(!timer_flag)
+        {
+          hal_timer = timerBegin(0, timer_divider, true);               // Create and Start timer (timer_number, divider, countUp)
+          timerAttachInterrupt(hal_timer, &timer_isr_handler, true);    // Provide ISR to timer (timer, void (*function)(), edge)
+          timerAlarmWrite(hal_timer, timer_max_count, false);           // At what count should ISR trigger (timer, count, autoreload)
+          timerAlarmEnable(hal_timer);                                  // Allow ISR to trigger
+
+          timer_flag = true;
+        }
 
         memset(recNum_c, 0, lengthOfNumber);
-        counterOfRecNumber = 0;
+        counterOfRecCharacter = 0;
       }
+    }
+  }
+}
+//<---- ------------------------------------------------------------------ ---->
+void echoSerial_Task(void* parameters)
+{
+  AvgData_t receivedAvgData;
+
+  while (1)
+  {
+    if(xQueueReceive(avg_msg_queue, (void*)&receivedAvgData, 0) == pdTRUE) 
+    {
+      Serial.println("<---- Hello from ECHO_Serial Task ---->");
+      Serial.print(receivedAvgData.message);
+      Serial.println(receivedAvgData.Number);
     }
   }
 }
